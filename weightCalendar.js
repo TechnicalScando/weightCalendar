1476c2ce9896e7dcba8b2405a55b65c55a125ba4
Gary <kikz270@gmail.com>
2022-02-07 09:59:25 -0600

    generated date squares and day squares, added most front end elements

--- weightCalendar.js
+++ weightCalendar.js
@@ -1,6 +1,35 @@
-const body = document.querySelector('body')
+const calendarBox = document.querySelector('.calendarBox')
+const weekBox = document.querySelector('.weekBox')
 
-const hello = document.createElement('h1')
-hello.textContent = 'hello world'
+// create the days of the week textboxes
+const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+for (const day of dayName) {
+  const dayBox = document.createElement('div')
+  dayBox.classList.add('dayBox')
 
-body.appendChild(hello)
+  dayBox.textContent = day
+  weekBox.appendChild(dayBox)
+}
+
+// create 35 date squares
+for (let i = 1; i <= 35; i++) {
+  const dateSquare = document.createElement('div')
+  dateSquare.classList.add('dateSquare')
+
+  const date = document.createElement('p')
+  date.classList.add('date')
+  date.textContent = i
+
+  const addButton = document.createElement('button')
+  addButton.classList.add('addButton')
+
+  const buttonImg = document.createElement('img')
+  buttonImg.src = './Images/add.png'
+
+  addButton.appendChild(buttonImg)
+
+  dateSquare.appendChild(date)
+  dateSquare.appendChild(addButton)
+
+  calendarBox.appendChild(dateSquare)
+}

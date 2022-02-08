e613ea94ba849d6a2211f1c40e49cac3ec467398
Gary <kikz270@gmail.com>
2022-02-08 12:44:54 -0600

    got the dates generated, began work on the calender system

--- weightCalendar.js
+++ weightCalendar.js
@@ -11,20 +11,20 @@ for (const day of dayName) {
   weekBox.appendChild(dayBox)
 }
 
-// create 35 date squares
-for (let i = 1; i <= 35; i++) {
+const createDateSquare = (index) => {
   const dateSquare = document.createElement('div')
   dateSquare.classList.add('dateSquare')
 
   const date = document.createElement('p')
   date.classList.add('date')
-  date.textContent = i
+  date.textContent = index
 
   const addButton = document.createElement('button')
   addButton.classList.add('addButton')
 
   const buttonImg = document.createElement('img')
-  buttonImg.src = './Images/add.png'
+  buttonImg.src = './Images/add.svg'
+  buttonImg.classList.add('addButtonImg')
 
   addButton.appendChild(buttonImg)
 
@@ -33,3 +33,49 @@ for (let i = 1; i <= 35; i++) {
 
   calendarBox.appendChild(dateSquare)
 }
+
+const createBlankDateSquare = () => {
+  const dateSquare = document.createElement('div')
+  dateSquare.classList.add('dateSquare')
+  calendarBox.appendChild(dateSquare)
+}
+
+// create 35 date squares
+const startDate = 6
+
+for (let i = 1; i <= 35; i++) {
+
+  if (i <= startDate) {
+    createBlankDateSquare()
+  } else {
+    createDateSquare(i - startDate)
+  }
+}
+
+const monthNames = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+  'October', 'November', 'December']
+
+const dayNumbers = ['31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31']
+
+const yearArray = []
+
+for (let i = 0; i < monthNames.length; i++) {
+  const monthArray = {
+    name: monthNames[i],
+    days: [],
+    startDay: 0
+  }
+  for (let x = 0; x < dayNumbers[i]; x++) {
+    if (x === 0) {
+      monthArray.startDay = new Date(2022, i, x + 1).getDay()
+    }
+    monthArray.days[x] = x + 1
+  }
+  yearArray.push(monthArray)
+}
+
+
+
+
+
+console.log(yearArray)
